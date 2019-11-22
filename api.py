8a96d600e97539de4573f40a8b1425efe727e1a3
wata-pom <w.kumagai1224@gmail.com>
2019-11-22 12:32:56 +0900

    楽天レシピAPI データ取得
    必要パラメーターをjson形式で取得した

--- api.py
+++ api.py
@@ -1,19 +1,13 @@
-import os
-
 import requests
 
-URL = 'https://api.gnavi.co.jp/RestSearchAPI/v3/'
-KEY_ID = os.environ['key_id']
+URL = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426'
 
+params = dict(applicationId='1013818012526051161',
+              formatversion=2,
+              categoryType='small',
+              elements='categoryName,categoryUrl')
 
-def main():
-    freeword = input('検索:')
-    response = requests.get(URL, params={'keyid': KEY_ID, 'freeword': freeword})
-    for i in range(0, 5):
-        print(response.json()['rest'][i]['name'])
-        print(response.json()['rest'][i]['url'])
-        print(response.json()['rest'][i]['access'])
+response = requests.get(URL, params)
+print(response.url)
 
-
-if __name__ == '__main__':
-    main()
+# def recie_search(food)
