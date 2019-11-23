3d3d937c920d7c0816fb3d94174c224f89c7772c
wata-pom <w.kumagai1224@gmail.com>
2019-11-23 10:14:52 +0900

    楽天レシピAPI データ取得
    キーワードからレシピURLを出力できた

--- api.py
+++ api.py
@@ -7,7 +7,18 @@ params = dict(applicationId='1013818012526051161',
               categoryType='small',
               elements='categoryName,categoryUrl')
 
-response = requests.get(URL, params)
-print(response.url)
 
-# def recie_search(food)
+def recipe_search(freeword='肉'):
+    response = requests.get(URL, params)
+    content = response.json()['result']['small']
+    for recipes in content:
+        recipe_name = recipes['categoryName']
+        recipe_url = recipes['categoryUrl']
+
+        if freeword in recipe_name:
+            return recipe_name, recipe_url
+
+    return 'キーワードを入力し直してください'
+
+
+print(recipe_search("さんま"))
