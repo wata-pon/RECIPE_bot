dda1418e5073853b2a6380f7bc8c00e0fe51c62a
wata-pom <w.kumagai1224@gmail.com>
2019-11-26 17:04:06 +0900

    カルーセルメッセージ テスト

--- api.py
+++ api.py
@@ -29,4 +29,4 @@ def recipe_rank():
 
 
 if __name__ == '__main__':
-    print(recipe_search('トマト')[0])
+    print(recipe_search('トマト'))
--- main.py
+++ main.py
@@ -7,8 +7,10 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage, CarouselTemplate, \
     CarouselColumn
 
-# import api
-# import scraping
+import api
+import scraping
+
+rank = scraping.recipe_scraping('')
 
 app = Flask(__name__)
 
@@ -46,45 +48,32 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    notes = [CarouselColumn(thumbnail_image_url="https://renttle.jp/static/img/renttle02.jpg",
-                            title="【ReleaseNote】トークルームを実装しました。",
-                            text="creation(創作中・考え中の何かしらのモノ・コト)に関して、意見を聞けるようにトークルーム機能を追加しました。",
-                            actions=[
-                                {"type": "message",
-                                 "label": "サイトURL",
-                                 "text": "https://renttle.jp/notes/kota/7"}
-                            ]),
+    notes = [CarouselColumn(image_url=rank[0]['img'],
+                            title=rank[0]['title'],
+                            actions=[{"type": "message", "label": "CLICK!", "text": rank[0]['url']}]),
 
-             CarouselColumn(thumbnail_image_url="https://renttle.jp/static/img/renttle03.jpg",
-                            title="ReleaseNote】創作中の活動を報告する機能を追加しました。",
-                            text="創作中や考え中の時点の活動を共有できる機能を追加しました。",
-                            actions=[
-                                {"type": "message",
-                                 "label": "サイトURL",
-                                 "text": "https://renttle.jp/notes/kota/6"}
-                            ]),
+             CarouselColumn(thumbnail_image_url=rank[1]['img'],
+                            title=rank[1]['title'],
+                            actions=[{"type": "message", "label": "CLICK!", "text": rank[1]['url']}]),
 
-             CarouselColumn(thumbnail_image_url="https://renttle.jp/static/img/renttle04.jpg",
-                            title="【ReleaseNote】タグ機能を追加しました。",
-                            text="「イベントを作成」「記事を投稿」「本を登録」にタグ機能を追加しました。",
-                            actions=[
-                                {"type": "message",
-                                 "label": "サイトURL",
-                                 "text": "https://renttle.jp/notes/kota/5"}]
-                            )]
+             CarouselColumn(thumbnail_image_url=rank[2]['img'],
+                            title=rank[2]['title'],
+                            actions=[{"type": "message", "label": "CLICK!", "text": rank[2]['url']}])
+             ]
 
     messages = TemplateSendMessage(
         alt_text='template',
         template=CarouselTemplate(columns=notes),
-    )
 
-    line_bot_api.reply_message(event.reply_token, messages=messages)
+    )
+    push_text = event.message.text,
+    api.recipe_search(push_text)
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(messages=messages))
 
     # push_text = event.message.text
     # msg = api.recipe_search(foodword=push_text)
     # line_bot_api.reply_message(event.reply_token,
     #                            TextSendMessage(text=msg))
-
 
 
 if __name__ == '__main__':
